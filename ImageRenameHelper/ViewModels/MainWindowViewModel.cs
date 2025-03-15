85c6491cfaf0853dab3da5087ff86b73f192bb3a
Akn <sibata486@gmail.com>
2025-03-16 00:33:38 +0900

    init /

---
+++ ImageRenameHelper/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using ImageRenameHelper.Utils;
+using Prism.Mvvm;
+
+namespace ImageRenameHelper.ViewModels
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class MainWindowViewModel : BindableBase
+    {
+        public MainWindowViewModel()
+        {
+            SetDummies();
+        }
+
+        public TextWrapper TextWrapper { get; set; } = new ();
+
+        [Conditional("DEBUG")]
+        private void SetDummies()
+        {
+        }
+    }
+}
