5d95c0a028c3a76d4bd28676f8315d2defc15868
Akn <sibata486@gmail.com>
2025-03-16 12:56:02 +0900

    add feat / アプリの起動時に新しいディレクトリを作成する機能を実装。

--- ImageRenameHelper/ViewModels/MainWindowViewModel.cs
+++ ImageRenameHelper/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
@@ -18,8 +19,29 @@ namespace ImageRenameHelper.ViewModels
         {
             PngInfoFileListViewModel = new FileListViewModel();
             ImageToImageTargetFileListViewModel = new FileListViewModel();
+
+            var workingDir = new DirectoryInfo("working-directories");
+            if (!workingDir.Exists)
+            {
+                Directory.CreateDirectory(workingDir.FullName);
+            }
+
+            CurrentDirectory = new DirectoryInfo(Path.Combine(workingDir.FullName, DateTime.Now.ToString("yyyyMMdd_HHmmss_fff")));
+            var pngInfoDir = Path.Combine(CurrentDirectory.FullName, "png-info-images");
+            var imagesDir = Path.Combine(CurrentDirectory.FullName, "target-images");
+
+            Directory.CreateDirectory(CurrentDirectory.FullName);
+            PngInfoFileListViewModel.CurrentDirectoryPath = Directory.CreateDirectory(pngInfoDir).FullName;
+            ImageToImageTargetFileListViewModel.CurrentDirectoryPath = Directory.CreateDirectory(imagesDir).FullName;
+
             SetDummies();
         }
+
+        /// <summary>
+        /// このアプリが作業中に取り扱う作業ディレクトリです。<br/>
+        /// PngInfo　のファイルと、 ImageToImage の対象ファイルを入れるディレクトリの２つを格納します。
+        /// </summary>
+        public DirectoryInfo CurrentDirectory { get; set; }
 
         public TextWrapper TextWrapper { get; set; } = new ();
 
