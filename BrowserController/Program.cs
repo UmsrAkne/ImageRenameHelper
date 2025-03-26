5ab295cd67067c4d55218c4f27450c42c6b97623
Akn <sibata486@gmail.com>
2025-03-27 02:35:15 +0900

    improve / 各 XPath を Selector に置き換えた。
    Full XPath では謎の呪文以外の何者でもなく、メンテが不可能であるため。

--- BrowserController/Program.cs
+++ BrowserController/Program.cs
@@ -5,7 +5,11 @@ namespace BrowserController
 {
     public class Program
     {
-        private static string topPageI2iTabSelector = "#tabs > div > button:nth-child(2)";
+        private static string topPageI2ITabSelector = "#tabs > div > button:nth-child(2)";
+        private static string i2IGenerationTabSelector = "#img2img_extra_tabs > div > button";
+        private static string i2IGenerationBatchTabSelector = "#mode_img2img > div.tab-nav.scroll-hide > button:nth-of-type(6)";
+        private static string i2IGenerationBatchFromDirTabSelector = "#img2img_batch_source > div.tab-nav.scroll-hide > button:nth-of-type(2)";
+        private static string pngInfoAreaToggleButtonSelector = "#component-894 > div.label-wrap > span:nth-child(1)";
 
         static void Main(string[] args)
         {
@@ -21,72 +25,73 @@ namespace BrowserController
             var driver = new ChromeDriver(options);
 
             // i2i tab
-            // ButtonClick(driver, "/html/body/gradio-app/div/div/div[1]/div/div/div[2]/div[1]/button[2]");
             ButtonClick(driver,
-                topPageI2iTabSelector,
+                topPageI2ITabSelector,
                 "i2i タブをクリック");
 
-            // Generation tab
-            ButtonClick(driver, "/html/body/gradio-app/div/div/div[1]/div/div/div[2]/div[3]/div/div[2]/div[1]/button[1]");
-
-            // Batch tab
-            ButtonClick(driver, "/html/body/gradio-app/div/div/div[1]/div/div/div[2]/div[3]/div/div[2]/div[2]/div/div/div[1]/div[1]/div[1]/button[6]");
-
-            // From Directory tab
-            ButtonClick(driver, "/html/body/gradio-app/div/div/div[1]/div/div/div[2]/div[3]/div/div[2]/div[2]/div/div/div[1]/div[1]/div[8]/div/div[1]/div[1]/button[2]");
 
             InputTextArea(driver,
-                "/html/body/gradio-app/div/div/div[1]/div/div/div[2]/div[3]/div/div[2]/div[2]/div/div/div[1]/div[1]/div[8]/div/div[1]/div[4]/div/div[2]/div[1]/label/textarea",
-                targetDirectory);
+                "#img2img_prompt > label > textarea",
+                string.Empty,
+                "Prompt　エリアをリセット"
+            );
+
+            InputTextArea(driver,
+                "#img2img_neg_prompt > label > textarea",
+                string.Empty,
+                "Negative Prompt　エリアをリセット"
+            );
+
+            // Generation tab
+            ButtonClick(driver, i2IGenerationTabSelector, "i2i.Generation タブをクリック");
+
+            // Batch tab
+            ButtonClick(driver, i2IGenerationBatchTabSelector, "i2i.Generation.Batch タブをクリック");
+
+            // From Directory tab
+            ButtonClick(driver, i2IGenerationBatchFromDirTabSelector, "i2i.Generation.Batch.FromDirectory タブをクリック");
+
+            InputTextArea(driver,
+                "#img2img_batch_input_dir > label > textarea",
+                targetDirectory,
+                "Input Directory テキストボックスに書き込み"
+            );
 
             // 'icon'クラスを持つspan要素を取得
-            var iconElement = driver.FindElement(By.XPath("/html/body/gradio-app/div/div/div[1]/div/div/div[2]/div[3]/div/div[2]/div[2]/div/div/div[1]/div[1]/div[8]/div/div[2]/div[2]/span[2]"));
+            var iconElement = driver.FindElement(By.CssSelector("#component-894 > div.label-wrap.svelte-s1r2yt > span.icon.svelte-s1r2yt"));
 
             // 'transform'プロパティの値を取得
             var transformValue = iconElement.GetCssValue("transform");
 
-            if (!transformValue.Contains("rotate(0deg)"))
+            if (!transformValue.Contains("matrix(1, 0, 0, 1, 0, 0)"))
             {
                 // PngInfo エリアの表示トグル
-                ButtonClick(driver, "/html/body/gradio-app/div/div/div[1]/div/div/div[2]/div[3]/div/div[2]/div[2]/div/div/div[1]/div[1]/div[8]/div/div[2]/div[2]");
+                ButtonClick(driver, pngInfoAreaToggleButtonSelector, "png 表示エリアのトグルボタンをクリック");
                 Thread.Sleep(400);
             }
 
+            System.Diagnostics.Debug.WriteLine($"一時停止(Program : 60)");
+
             // PNG Info directory
             InputTextArea(driver,
-                "/html/body/gradio-app/div/div/div[1]/div/div/div[2]/div[3]/div/div[2]/div[2]/div/div/div[1]/div[1]/div[8]/div/div[2]/div[3]/div/div/div[2]/label/textarea",
-                pngInfoDirectory);
+                "#img2img_batch_png_info_dir > label > textarea",
+                pngInfoDirectory,
+                "png info ディレクトリのパスを入力");
 
             // Append Png info to prompts
-            ChangeCheckBoxState(driver,
-                "/html/body/gradio-app/div/div/div[1]/div/div/div[2]/div[3]/div/div[2]/div[2]/div/div/div[1]/div[1]/div[8]/div/div[2]/div[3]/div/div/div[1]/label/input",
-                true);
+            ChangeCheckBoxState(driver, "#img2img_batch_use_png_info > label > input", true);
 
             // Prompt
-            ChangeCheckBoxState(driver,
-                "/html/body/gradio-app/div/div/div[1]/div/div/div[2]/div[3]/div/div[2]/div[2]/div/div/div[1]/div[1]/div[8]/div/div[2]/div[3]/div/div/fieldset/div[3]/label[1]/input",
-                true);
+            ChangeCheckBoxState(driver, "#component-897 > div.wrap > label:nth-child(1) > input", true);
 
             // Negative Prompt
-            ChangeCheckBoxState(driver,
-                "/html/body/gradio-app/div/div/div[1]/div/div/div[2]/div[3]/div/div[2]/div[2]/div/div/div[1]/div[1]/div[8]/div/div[2]/div[3]/div/div/fieldset/div[3]/label[2]/input",
-                true);
+            ChangeCheckBoxState(driver, "#component-897 > div.wrap > label:nth-child(2) > input", true);
 
             // Seed
-            ChangeCheckBoxState(driver,
-                "/html/body/gradio-app/div/div/div[1]/div/div/div[2]/div[3]/div/div[2]/div[2]/div/div/div[1]/div[1]/div[8]/div/div[2]/div[3]/div/div/fieldset/div[3]/label[3]/input",
-                true);
+            ChangeCheckBoxState(driver, "#component-897 > div.wrap > label:nth-child(3) > input", true);
 
             driver.Quit();
             Console.WriteLine("処理を終了します");
-        }
-
-        private static void ButtonClick(IWebDriver driver, string fullXPath)
-        {
-            var button = driver.FindElement(By.XPath(fullXPath));
-            button.Click();
-
-            Thread.Sleep(200);
         }
 
         private static void ButtonClick(IWebDriver driver, string cssSelector, string consoleMessage)
@@ -98,22 +103,25 @@ namespace BrowserController
             Thread.Sleep(200);
         }
 
-        private static void InputTextArea(IWebDriver driver, string fullXPath, string inputText)
+        private static void InputTextArea(IWebDriver driver, string selector, string inputText, string consoleMessage)
         {
-            var textArea = driver.FindElement(By.XPath(fullXPath));
+            var textArea = driver.FindElement(By.CssSelector(selector));
             textArea.Clear();
             textArea.SendKeys(inputText);
+            Console.WriteLine(consoleMessage);
 
             Thread.Sleep(200);
         }
 
-        private static void ChangeCheckBoxState(IWebDriver driver, string fullXPath, bool state)
+        private static void ChangeCheckBoxState(IWebDriver driver, string selector, bool state)
         {
-            var checkBox = driver.FindElement(By.XPath(fullXPath));
+            var checkBox = driver.FindElement(By.CssSelector(selector));
             if (checkBox.Selected == state)
             {
                 return;
             }
+
+            Console.WriteLine($"{selector}(Click : 110)");
 
             checkBox.Click();  // チェックボックスをオンにする
             Thread.Sleep(200);
