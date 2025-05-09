61ad7a4398d6c87e3a266dae3bac4823296684a4
UmsrAkne <sibata486@gmail.com>
2025-05-09 20:42:13 +0900

    feat: add global exception logging for UI and background threads

--- ImageRenameHelper/App.xaml.cs
+++ ImageRenameHelper/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.IO;
+using System.Windows;
 using ImageRenameHelper.ViewModels;
 using ImageRenameHelper.Views;
 using Prism.Ioc;
@@ -18,6 +20,34 @@ namespace ImageRenameHelper
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
             containerRegistry.RegisterDialog<WorkingDirectoryChangePage, WorkingDirectoryChangePageViewModel>();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // For UI thread
+            DispatcherUnhandledException += (_, args) =>
+            {
+                LogFatal(args.Exception); // Log the exception when the application crashes.
+
+                // Let the application crash. Set explicitly for clarity, although 'false' is the default.
+                args.Handled = false;
+            };
+
+            // For Background thread
+            AppDomain.CurrentDomain.UnhandledException += (_, args) =>
+            {
+                if (args.ExceptionObject is Exception ex)
+                {
+                    LogFatal(ex);
+                }
+            };
+
+            base.OnStartup(e);
+        }
+
+        private void LogFatal(Exception ex)
+        {
+            File.AppendAllText("crash.log", $"[{DateTime.Now}] {ex}\n\n");
         }
     }
 }
